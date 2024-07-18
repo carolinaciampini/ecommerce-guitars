a2add76b9f967cd5713bfb2d5530fdc3aa386522
carolinaciampini <140980993+carolinaciampini@users.noreply.github.com>
2024-07-18 16:22:26 -0300

    GuitarCard

---
+++ ecommerceGuitars/src/components/Guitar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Guitar({ guitar }) {  // Asegúrate de recibir `guitar` como prop
+  if (!guitar) {
+    return null;  // Si no se pasa el prop `guitar`, no renderizar nada
+  }
+
+  return (
+    <div className="col-md-6 col-lg-4 my-4 row align-items-center">
+      <div className="col-4">
+        <img
+          className="img-fluid"
+          src={`/img/${guitar.image}.jpg`}
+          alt={guitar.name}
+        />
+      </div>
+      <div className="col-8">
+        <h3 className="text-black fs-4 fw-bold text-uppercase">{guitar.name}</h3>
+        <p>{guitar.description}</p>
+        <p className="fw-black text-primary fs-3">
+          <strong>Price:</strong> ${guitar.price}
+        </p>
+        <button
+          type="button"
+          className="btn btn-dark w-100"
+        >
+          Agregar al Carrito
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Guitar;
